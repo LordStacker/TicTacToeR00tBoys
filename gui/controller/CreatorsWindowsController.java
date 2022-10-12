4c218c5506b3ae0c5f1916187dbecdb3e7737dd4
TomassSimko <simko.t@email.cz>
2022-10-12 14:42:43 +0200

    added creators view

---
+++ gui/controller/CreatorsWindowsController.java
@@ -0,0 +1,50 @@
+package tictactoe.gui.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CreatorsWindowsController implements Initializable {
+    @FXML
+    private Button closeButton;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        closeButton.setOnAction(this::setCloseButton);
+
+    }
+    @FXML
+    private void setCloseButton(ActionEvent event){
+        Stage stage = (Stage) closeButton.getScene().getWindow();
+        stage.close();
+    }
+
+    //Making hyperlinks
+    @FXML
+    public void openNicoGit(ActionEvent actionEvent) throws URISyntaxException, IOException {
+        Desktop.getDesktop().browse(new URI("https://github.com/LordStacker"));
+    }
+    @FXML
+    public void openTomasGIt(ActionEvent actionEvent) throws URISyntaxException, IOException {
+        Desktop.getDesktop().browse(new URI("https://github.com/TomassSimko"));
+    }
+    @FXML
+    public void openDimiGit(ActionEvent actionEvent) throws URISyntaxException, IOException {
+        Desktop.getDesktop().browse(new URI("https://github.com/DimitarGateshki"));
+    }
+    @FXML
+    public void openAtanasGit(ActionEvent actionEvent) throws URISyntaxException, IOException {
+        Desktop.getDesktop().browse(new URI("https://github.com/AtanasLS"));
+    }
+
+
+}
